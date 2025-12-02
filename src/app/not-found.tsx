43674291fce15d2c60b9ee22be5bbd2ee2165932
JM Catamora <lucius.castus07@gmail.com>
2025-12-02 21:18:05 +0800

    commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function Page404() {
+  return (
+    <div className="error-404">
+      <div>
+        <img src="/404-image.png"/>
+      </div>
+      <p>Sorry, the page you’re looking for doesn't exist.</p>
+    </div>
+  );
+}
